3633fd7cfd49190236f54a85f88ed55859e77e57
Kevin Gomez <kevingomezpersonal@gmail.com>
2021-07-14 14:38:45 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+require("dotenv").config();
+
+//initalize express
+const app = express();
+
+//initalize port
+const SERVER_PORT = process.env.PORT || 5002;
+
+//initalize middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan(":method :url :response-time"));
+
+//MonogoDB connect
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+//import routes
+
+//initalize routes
+
+//basic greeting
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+//app listening
+app.listen(SERVER_PORT, () => {
+  console.log(`Server running at ${SERVER_PORT}`);
+});
